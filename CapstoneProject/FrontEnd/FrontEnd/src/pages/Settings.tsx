edd0e9e31e2112d582c3449b13c3c74697f2d303
Çiğdem Çakır <102484836+medgicakir@users.noreply.github.com>
2023-07-09 10:12:11 +0300

    Created settings and live logs page (to be edited).

--- CapstoneProject/FrontEnd/FrontEnd/src/pages/Settings.tsx
+++ CapstoneProject/FrontEnd/FrontEnd/src/pages/Settings.tsx
@@ -1,5 +1,38 @@
-function Settings() {
-    return <h2>Settings</h2>;
+import { useState, ChangeEvent, FormEvent } from 'react';
+
+function SettingsPage() {
+    const [notificationType, setNotificationType] = useState('email');
+
+    const handleNotificationChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setNotificationType(e.target.value);
+    };
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        // Burada backend ile iletişime geçip ayarları kaydedebilirsiniz.
+        console.log(`Settings saved! Notification type: ${notificationType}`);
+    };
+
+    return (
+        <div>
+            <h1>Settings Page</h1>
+            <form onSubmit={handleSubmit}>
+                <div style={{ marginBottom: '20px' }}>
+                    <label>
+                        Notification Type:
+                        <select value={notificationType} onChange={handleNotificationChange}>
+                            <option value="email">E-Posta</option>
+                            <option value="in-app">Uygulama İçi Notifikasyon</option>
+                            <option value="none">Hiçbiri</option>
+                        </select>
+                    </label>
+                </div>
+                <div style={{ marginBottom: '20px' }}>
+                    <input type="submit" value="Save Settings" />
+                </div>
+            </form>
+        </div>
+    );
 }
 
-export default Settings;
+export default SettingsPage;
--- CapstoneProject/FrontEnd/FrontEnd/src/pages/Users.tsx
+++ CapstoneProject/FrontEnd/FrontEnd/src/pages/Users.tsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from 'react';
+
 function Users() {
-    return <h2>Users</h2>;
+    const [logs, setLogs] = useState<string[]>([]);
+
+    useEffect(() => {
+        const fakeLogs = [
+            'Sipariş oluşturuldu...',
+            'Sipariş bilgileri doğrulanıyor...',
+            'Ürünler kazıma işlemi için sıraya alındı...',
+            'Kazıma işlemi başladı...',
+            'Kazıma işlemi tamamlandı. Ürün bilgileri alındı...',
+            'Sipariş tamamlandı!'
+        ];
+
+        const addLog = (index: number) => {
+            if (index < fakeLogs.length) {
+                setLogs((prevLogs) => [...prevLogs, `${new Date().toLocaleString()}: ${fakeLogs[index]}`]);
+                setTimeout(() => addLog(index + 1), 2000);
+            }
+        };
+
+        addLog(0);
+    }, []);
+
+    return (
+        <div>
+            <h1>Live Log Page</h1>
+            <div style={{ backgroundColor: 'black', color: 'lime', padding: '10px', fontFamily: 'Courier', whiteSpace: 'pre-line' }}>
+                {logs.join('\n')}
+            </div>
+        </div>
+    );
 }
 
 export default Users;
