5c3b5f79a9555bc6e753ebc20ba80a0493c124f9
Çiğdem Çakır <102484836+medgicakir@users.noreply.github.com>
2023-07-18 12:51:25 +0300

    Second migration.

---
+++ CapstoneProject/BackEnd/SoftwareHouse/src/Infrastrusture/Persistence/Migrations/Application/20230718094139_SecondCreate.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastrusture.Persistence.Migrations.Application
+{
+    /// <inheritdoc />
+    public partial class SecondCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedByUserId",
+                table: "Products",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedByUserId",
+                table: "Products",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedByUserId",
+                table: "Products",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedByUserId",
+                table: "Orders",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedByUserId",
+                table: "Orders",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedByUserId",
+                table: "Orders",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Orders",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedByUserId",
+                table: "OrderEvents",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedByUserId",
+                table: "OrderEvents",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedByUserId",
+                table: "OrderEvents",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedByUserId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedByUserId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedByUserId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedByUserId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedByUserId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedByUserId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedByUserId",
+                table: "OrderEvents");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedByUserId",
+                table: "OrderEvents");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedByUserId",
+                table: "OrderEvents");
+        }
+    }
+}
